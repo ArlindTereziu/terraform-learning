00d40fa4c76fb968258b4cb3e5f283b34fad7f9d
Arlind Tereziu <arlind.tereziu@gmail.com>
2023-01-18 11:54:06 +0100

    Cleanup

--- main.tf
+++ main.tf
@@ -13,21 +13,9 @@ terraform {
       name = "gh-actions"
     }
   }
-  #   backend "azurerm" {
-  #     resource_group_name  = "rg-tfstate"
-  #     storage_account_name = "attfstatefile"
-  #     container_name       = "tfstatefiles"
-  #     key                  = "tfgitaction.tfstate"
-  #   }
-
   required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
   features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-test-01"
-  location = "westeurope"
 }
