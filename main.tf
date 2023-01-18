78b62c98cb6637e6e1330670f94dbb57fad991a7
Arlind Tereziu <arlind.tereziu@gmail.com>
2023-01-18 12:32:09 +0100

    Crate VNET

--- main.tf
+++ main.tf
@@ -24,7 +24,42 @@ resource "azurerm_resource_group" "rg-networking" {
   location = "West Europe"
   tags = {
     environment = "production"
-    source = "terraform"
-    project = "lab"
+    source      = "terraform"
+    project     = "lab"
+  }
+}
+resource "azurerm_network_security_group" "nsg-networking" {
+  name                = "nsg-networking"
+  location            = azurerm_resource_group.rg-networking.location
+  resource_group_name = azurerm_resource_group.rg-networking.name
+  tags = {
+    environment = "production"
+    source      = "terraform"
+    project     = "lab"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet-lan" {
+  name                = "vnet-lan"
+  location            = azurerm_resource_group.rg-networking.location
+  resource_group_name = azurerm_resource_group.rg-networking.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.nsg-networking.id
+  }
+
+  tags = {
+    environment = "production"
+    source      = "terraform"
+    project     = "lab"
   }
 }
